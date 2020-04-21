f360a0c3242497971faf0c01d7115f2b0e667430
DJ Shamblin <hamblindj@gmail.com>
2020-04-20 21:21:14 -0700

    chore: release v3.0.2 (#3)
    * test shipjs
    
    * chore: prepare v3.0.2

--- ship.config.js
+++ ship.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   publishCommand: ({ defaultCommand }) => `${defaultCommand} --access public`,
+  mergeStrategy: {
+    toReleaseBranch: {
+      develop: 'master'
+    }
+  }
 };
